e14bd6283a0b910a2f31654c74fae8351959bf93
Issa-hub14 <isabellaquijano1@gmail.com>
2026-04-13 11:01:21 -0500

    Creación atributos - constructor - getters y setters de la clase Persona

--- src/main/java/Modelo/Persona.java
+++ src/main/java/Modelo/Persona.java
@@ -9,5 +9,48 @@ package Modelo;
  * @author isabe
  */
 public class Persona {
+    protected String nombre; 
+    protected String direccion;
+    protected int telefono;
+    protected Fecha fechaDeNacimiento;
+    
+    public Persona(String nombre, String direccion, int telefono, Fecha fechaDeNacimiento){
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public Fecha getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(Fecha fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
     
 }
