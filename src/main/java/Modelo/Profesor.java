3a8da6491b4740357221b1471747d7066dd7d9ad
Issa-hub14 <isabellaquijano1@gmail.com>
2026-04-13 14:29:23 -0500

    Creación de atributos,constructor, getters y setters, y métodos de la clase Profesor

--- src/main/java/Modelo/Profesor.java
+++ src/main/java/Modelo/Profesor.java
@@ -8,6 +8,88 @@ package Modelo;
  *
  * @author isabe
  */
-public class Profesor extends Persona{
+public class Profesor extends Persona {
+
     private int cedula;
+    private String area;
+    private double salarioHora;
+    private int horasMes;
+
+    public Profesor(String nombre, String direccion, String telefono, Fecha fechaDeNacimiento,
+            int cedula, String area, double salarioHora, int horasMes) {
+
+        super(nombre, direccion, telefono, fechaDeNacimiento);
+
+        this.cedula = cedula;
+        this.area = area;
+        this.salarioHora = salarioHora;
+        this.horasMes = horasMes;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public double getSalarioHora() {
+        return salarioHora;
+    }
+
+    public void setSalarioHora(double salarioHora) {
+        this.salarioHora = salarioHora;
+    }
+
+    public int getHorasMes() {
+        return horasMes;
+    }
+
+    public void setHorasMes(int horasMes) {
+        this.horasMes = horasMes;
+    }
+
+    public double calcularSalarioTotal() {
+        double salarioBase = salarioHora * horasMes;
+        double salarioTotal = salarioBase + (salarioBase * 0.30);
+        return salarioTotal;
+    }
+
+    public double calcularPrestaciones() {
+        double prestaciones = calcularSalarioTotal() * 0.19;
+        return prestaciones;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()
+                + " | Cargo: Profesor"
+                + " | Cédula: " + cedula
+                + " | Área: " + area
+                + " | Salario total: " + calcularSalarioTotal()
+                + " | Prestaciones: " + calcularPrestaciones();
+    }
+
+    public String obtenerDatosProfesor() {
+        return "Profesor: " + nombre
+                + "\nÁrea: " + area
+                + "\nSalario total: " + calcularSalarioTotal()
+                + "\nPrestaciones: " + calcularPrestaciones();
+    }
+
+    public String obtenerDatosProfesor(String areaBuscada) {
+        if (this.area.equalsIgnoreCase(areaBuscada)) {
+            return obtenerDatosProfesor();
+        }
+        return "El profesor " + nombre + " no pertenece al área " + areaBuscada;
+    }
 }
